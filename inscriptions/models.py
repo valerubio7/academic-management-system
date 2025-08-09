18a5ac9f07e7ebc1dec83f3fa28263880199254d
Valentin Rubio <rubiovalentin01@gmail.com>
2025-08-09 11:28:22 -0300

    feat(inscriptions): create models for exam and subject inscriptions

--- inscriptions/models.py
+++ inscriptions/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class SubjectInscription(models.Model):
+    """Model representing a subject inscription."""
+    student = models.ForeignKey('users.Student', on_delete=models.CASCADE, related_name='subjects_inscriptions')
+    subject = models.ForeignKey('academics.Subject', on_delete=models.CASCADE, related_name='subject_inscriptions')
+    inscription_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student.username} - {self.subject.name} ({self.inscription_date})"
+
+
+class FinalExamInscription(models.Model):
+    """Model representing a final exam inscription."""
+    student = models.ForeignKey('users.Student', on_delete=models.CASCADE, related_name='final_exam_inscriptions')
+    final_exam = models.ForeignKey('academics.FinalExam', on_delete=models.CASCADE, related_name='final_exam_inscriptions')
+    inscription_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student.username} - {self.final_exam.subject.name} ({self.inscription_date})"
