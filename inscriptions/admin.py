f5359b652ed550d471d94d54aa2da62b0222fbeb
Valentin Rubio <rubiovalentin01@gmail.com>
2025-08-11 16:21:32 -0300

    feat(inscriptions): add admin interface for inscriptions models

--- inscriptions/admin.py
+++ inscriptions/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import SubjectInscription, FinalExamInscription
 
-# Register your models here.
+
+@admin.register(SubjectInscription)
+class SubjectInscriptionAdmin(admin.ModelAdmin):
+    list_display = ("student", "subject", "inscription_date")
+    search_fields = ("student__user__username", "subject__name", "subject__code")
+
+
+@admin.register(FinalExamInscription)
+class FinalExamInscriptionAdmin(admin.ModelAdmin):
+    list_display = ("student", "final_exam", "inscription_date")
+    search_fields = ("student__user__username", "final_exam__subject__name", "final_exam__subject__code")
