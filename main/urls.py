f5b90134065bc0453d5f06b7533d5f67806a106e
Valentin Rubio <rubiovalentin01@gmail.com>
2025-08-11 19:30:59 -0300

    feat(main): add home view and include accounts URLs in the main URL configuration

--- main/urls.py
+++ main/urls.py
@@ -18,7 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # This line serves media files during development
