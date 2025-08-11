a371cf3befd5e257bceb6878ed46bd3a2e875841
Valentin Rubio <rubiovalentin01@gmail.com>
2025-08-11 16:22:04 -0300

    feat(users): add admin interface for user models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import CustomUser, Student, Professor, Administrator
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "role", "dni", "email")
+    search_fields = ("username", "dni", "email")
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "user", "career", "enrollment_date")
+    search_fields = ("student_id", "user__username", "career__name")
+
+
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ("professor_id", "user", "degree", "category", "hire_date")
+    search_fields = ("professor_id", "user__username", "degree")
+
+
+@admin.register(Administrator)
+class AdministratorAdmin(admin.ModelAdmin):
+    list_display = ("administrator_id", "user", "position", "hire_date")
+    search_fields = ("administrator_id", "user__username", "position")
